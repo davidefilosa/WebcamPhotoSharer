54fcc709c5d72158a5783ce2e0650ad42e84e84b
davidefilosa <52102667+davidefilosa@users.noreply.github.com>
2022-11-22 10:11:05 +0100

    Update fileshare.py

--- fileshare.py
+++ fileshare.py
@@ -1,7 +1,7 @@
 from filestack import Client
 
 class FileSharer:
-    def __init__(self, filepath, api_key='AhzQO8uKTZOpkvyprZ8p3z'):
+    def __init__(self, filepath, api_key=''):
         self.filepath = filepath
         self.api_key = api_key
 

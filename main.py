402fb2751ef6782fa1be2ecbb8d05b34346a5da7
Davide Filosa <davideclaypool@gmail.com!>
2022-11-22 09:02:53 +0100

    Added docstring to the classes

--- main.py
+++ main.py
@@ -9,16 +9,20 @@ Builder.load_file('frontend.kv')
 
 class CameraScreen(Screen):
     def start(self):
+        """Starts camera and change Button text"""
         self.ids.camera.play = True
         self.ids.start.text = 'Stop Camera'
         self.ids.camera.texture = self.ids.camera.texture
 
     def stop(self):
+        """Stops camera and change Button text"""
         self.ids.camera.play = False
         self.ids.start.text = 'Start Camera'
         self.ids.camera.texture = None
 
     def capture(self):
+        """Create a file name with the current time
+        stamp and save an image under that filename"""
         time_stamp = time.strftime("%Y%m%d-%H%M%S")
         filepath = f'files/{time_stamp}.png'
         self.ids.camera.export_to_png(filepath)
